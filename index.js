0bf286712b82fc5ccc73a57b86d3a52652f64cc9
mixmix <mix@protozoa.nz>
2024-04-03 16:13:17 +1300

    add patches, extract plugins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const env = require('ahau-env')()
 
 const Config = require('./ssb.config')
+const plugins = require('./ssb.plugins')
 const karakia = require('./karakia')
 const noop = () => {}
 
@@ -9,47 +10,7 @@ module.exports = function startPataka (opts) {
   karakia()
 
   const stack = require('secret-stack')(env.pataka.caps)
-    .use(require('ssb-db'))
-    // .use(require('ssb-master'))
-    // .use(require('ssb-unix-socket'))
-    // .use(require('ssb-no-auth'))
-
-    .use(require('ssb-conn'))
-    .use(require('ssb-lan'))
-    .use(require('ssb-replicate'))
-    .use(require('ssb-friends'))
-
-    .use(require('ssb-blobs'))
-    .use(require('ssb-serve-blobs'))
-    .use(require('ssb-hyper-blobs'))
-
-    .use(require('ssb-query'))
-    .use(require('ssb-backlinks'))
-
-    .use(require('ssb-settings'))
-    .use(require('ssb-profile'))
-    // .use(require('ssb-story'))
-    // .use(require('ssb-artefact'))
-    // .use(require('ssb-whakapapa'))
-
-    .use(require('ssb-invite'))
-
-    // HACK 2023-03-13 (mix) - we don't want ssb-tribes installing boxers/ unboxers
-    // as this slows down the pataka. We do want some of the tribes APIs for graphql
-    // queries
-    .use({
-      ...require('ssb-tribes'),
-      init: (ssb, config) => {
-        const _ssb = {
-          ...ssb,
-          addBoxer: noop,
-          addUnboxer: noop
-        }
-        return require('ssb-tribes').init(_ssb, config)
-      }
-    })
-    .use(require('ssb-recps-guard'))
-    .use(require('ssb-pataka'))
+    .use(plugins)
 
   const config = Config(opts)
   const server = stack(config)
